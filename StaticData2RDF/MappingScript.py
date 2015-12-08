e7b24a281c45ce9405f2bbdeeffe0e143a704768
ivodeliefde <ivodeliefde@gmail.com>
2015-12-08 16:41:49 +0100

    test script for writing provinces from Postgres to RDF

--- StaticData2RDF/MappingScript.py
+++ StaticData2RDF/MappingScript.py
@@ -1,26 +1,35 @@
 from rdflib import URIRef, BNode, Literal, Graph
+from rdflib.namespace import RDF
+import rdflib
+import psycopg2
 
-from rdflib.namespace import RDF, FOAF
+def getData(dbms_name, user, password):
+	conn = psycopg2.connect(host="localhost", port='5433', database=dbms_name, user=user, password=password) 
+	cur = conn.cursor()
 
-RDF.type
-# = rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+	cur.execute("select provincien, ST_astext(geom) from provinces")
+	table = cur.fetchall()
+	return table
 
-FOAF.knows
-# = rdflib.term.URIRef(u'http://xmlns.com/foaf/0.1/knows')
+def table2RDF(table):
+	RDF.type 
+	# = rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
 
-bob = URIRef("http://example.org/people/Bob")
-linda = BNode() # a GUID is generated
+	geom = rdflib.Namespace("http://geovocab.org/geometry#")
+	dbpedia = rdflib.Namespace("http://dbpedia/resource/")
 
-name = Literal('Bob') # passing a string
-age = Literal(24) # passing a python int
-height = Literal(76.5) # passing a python float
+	g = Graph()
 
-g = Graph()
+	for province in provinces:
+		name = Literal(province[0])
+		geometry = Literal(province[1])
+		g.add( (name, RDF.type, dbpedia.Province) )
+		g.add( (geometry, RDF.type, geom.MultiPolygon) )
+		g.add( (name, geom.Geometry, geometry) )
+	g.serialize("test.rdf", format='turtle')
 
-g.add( (bob, RDF.type, FOAF.Person) )
-g.add( (bob, FOAF.name, name) )
-g.add( (bob, FOAF.knows, linda) )
-g.add( (linda, RDF.type, FOAF.Person) )
-g.add( (linda, FOAF.name, Literal('Linda') ) )
 
-print g.serialize(format='turtle')
+if (__name__ == "__main__"):
+	provinces = getData("Masterthesis", "postgres", "")
+	table2RDF(provinces)
+
