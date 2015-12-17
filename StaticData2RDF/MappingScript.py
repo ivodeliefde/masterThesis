ffc63e38776d48c104a679fb2f1a81b66cd773df
ivodeliefde <ivodeliefde@gmail.com>
2015-12-17 13:00:42 +0100

    added FOAF for names and country parameters (since data will not only be about the Netherlands, but also about Belgium)

--- StaticData2RDF/MappingScript.py
+++ StaticData2RDF/MappingScript.py
@@ -1,5 +1,5 @@
 from rdflib import URIRef, BNode, Literal, Graph
-from rdflib.namespace import RDF, RDFS
+from rdflib.namespace import RDF, RDFS, FOAF
 import rdflib
 import psycopg2
 
@@ -15,7 +15,7 @@ def getData(dbms_name, user, password):
 	return table
 
 # table2RDF takes a table with spatial objects which have a name and a geometry as input and creates an RDF file from it
-def table2RDF(table):
+def table2RDF(table, country):
 	# GeoSPARQL vocabulary
 	geom = rdflib.Namespace("http://www.opengis.net/ont/geosparql#")
 	# DBPedia
@@ -23,21 +23,27 @@ def table2RDF(table):
 
 	# Create a graph
 	g = Graph()
+	
+	try:
+		g.parse("test.ttl", format="turtle")
+		#print g.serialize(format='turtle')
+	except:
+		pass
 
 	# Add provinces with links to the graph
 	for row in table:
 		# Name and geometry should still get URIs assigned to them
-		thing = URIRef('http://example.com/netherlands/province/' + row[0].lower() )
-		geometry = URIRef('http://example.com/netherlands/province/' + row[0].lower() + '#geometry')
+		thing = URIRef('http://example.com/{1}/province/{1}'.format(country, row[0].lower() ) )
+		geometry = URIRef('http://example.com/{0}/province/{1}#geometry'.format(country, row[0].lower() ) )
 		# Create links
 		g.add( (thing, RDF.type, dbpedia.Province) )
-		g.add( (geometry, RDF.type, geom.Geometry ) )
-		g.add( (thing, RDFS.label, Literal(row[0]) ) )
+		g.add( (geometry, RDF.type, geom.Geometry) )
+		g.add( (thing, FOAF.name, Literal(row[0])) )
 		g.add( (geometry, RDFS.Datatype, geom.wktLiteral) )
 		g.add( (geometry, RDFS.Literal, Literal(row[1]) ) )
-		g.add( (thing, geom.hasGeometry, geometry ) )
+		g.add( (thing, geom.hasGeometry, geometry) )
 		
-	print g.serialize(format='turtle')
+	#print g.serialize(format='turtle')
 
 	# Write the graph to a RDF file in the turtle format
 	g.serialize("test.ttl", format='turtle')
@@ -46,5 +52,5 @@ def table2RDF(table):
 
 if (__name__ == "__main__"):
 	provinces = getData("Masterthesis", "postgres", "")
-	table2RDF(provinces)
+	table2RDF(provinces, 'Netherlands')
 
