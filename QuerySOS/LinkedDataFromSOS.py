db6731f1551aeda0c9b3e6e6365258f2d809094d
ivodeliefde <ivodeliefde@gmail.com>
2016-01-19 20:43:08 +0100

    added the function to retrieve data from a SOS in the PyWPS template

---
+++ QuerySOS/LinkedDataFromSOS.py
@@ -0,0 +1,49 @@
+from pywps.Process import WPSProcess
+from wpsRequests import Request    
+
+class Process(WPSProcess):
+
+
+    def __init__(self):
+
+        #----------------------------------------------------------------------#
+        # Process initialization
+        #----------------------------------------------------------------------#
+
+        WPSProcess.__init__(self,
+            identifier = "LinkedDataFromSOS",
+            title="Creates Linked Data of SOS metadata",
+            abstract="""This process takes an HTTP address of a Sensor Observation Service (SOS) as input and converts the metadata to linked data.""",
+            version = "1.0",
+            storeSupported = True,
+            statusSupported = True)
+
+        #----------------------------------------------------------------------#
+        # Adding process inputs
+        #----------------------------------------------------------------------#
+
+        self.textIn = self.addLiteralInput(identifier="text",
+                    title = "Input a string containing an HTTP address of a Sensor Observation Service (SOS). For example: 'http://someaddress.com/sos?'")
+
+        #----------------------------------------------------------------------#
+        # Adding process outputs
+        #----------------------------------------------------------------------#
+
+        self.textOut = self.addLiteralOutput(identifier = "text",
+                title="Output literal data")
+
+    #--------------------------------------------------------------------------#
+    # Execution part of the process
+    #--------------------------------------------------------------------------#
+
+    def execute(self):
+
+        url = self.textIn.getValue()
+
+        organisation, costs, acccesConstraints, minTime, featureofinterest, observableproperty, offerings = Request(url)
+
+        # make linked data from the data retrieved above
+
+        self.textOut.setValue( 'The WPS has finished' )
+
+        return
