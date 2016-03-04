853e17fd0fe66787a4ebe8dc9881c6638da016a5
ivodeliefde <ivodeliefde@gmail.com>
2016-03-04 14:28:49 +0100

    storing rdf documents via POST instead of INSERT DATA queries

--- WPS1/linkedDataCapabilities.py
+++ WPS1/linkedDataCapabilities.py
@@ -1,4 +1,4 @@
-import logging
+import os
 from rdflib import URIRef, BNode, Literal, Graph
 from rdflib.namespace import RDF, RDFS, FOAF
 import rdflib
@@ -7,11 +7,12 @@ from lxml import etree
 from postPURLS import CreatePurls, postPURLbatch
 import  progressbar
 
-logging.basicConfig()
+import logging
+# logging.basicConfig(level=logging.INFO)
 
 baseURI = "http://localhost:8099/masterThesis_tudelft" 
 # endpoint = 'http://localhost:8089/parliament/sparql?' 
-endpoint = "http://localhost/strabon-endpoint-3.3.2-SNAPSHOT/Query"
+endpoint = "http://localhost:80/strabon-endpoint-3.3.2-SNAPSHOT/Query"
 purlBatch = 'D:/purlBatches/SOS/SOSbatch'
 
 def capabilities(SOS):
@@ -44,11 +45,11 @@ def capabilities(SOS):
 	for version in SOS.version:
 		g.add( ( uriSOS, dc.hasVersion, Literal(version) ) )
 	for format in SOS.responseFormat:
-		uriFormat = URIRef(format)
+		uriFormat = URIRef(format.replace(' ',''))
 		g.add( ( uriSOS, dc.hasFormat, uriFormat ) )
 		g.add( ( uriFormat, RDFS.label, Literal('responseFormat') ) )
 	for format in SOS.resourceDescriptionFormat:
-		uriFormat = URIRef(format)
+		uriFormat = URIRef(format.replace(' ',''))
 		g.add( ( uriSOS, dc.hasFormat, uriFormat ) )
 		g.add( ( uriFormat, RDFS.label, Literal('resourceDescriptionFormat') ) )
 
@@ -66,20 +67,20 @@ def capabilities(SOS):
 			count += 1
 			bar.update(count)
 
-			if (proc[:4].lower() == 'http'):
-				uriProcedure = URIRef(proc)
-			else:
-				uriProcedure = URIRef("{0}/PROC/{1}".format(baseURI, count) ) 
-				# Create a PURL for every procedure URI 
-				CreatePurls([uriProcedure], purlBatch)
+			# if (proc[:4].lower() == 'http'):
+			# 	uriProcedure = URIRef(proc)
+			# else:
+			uriProcedure = URIRef("{0}/PROC/{1}".format(baseURI, count).replace(' ','') ) 
+			# Create a PURL for every procedure URI 
+			CreatePurls([uriProcedure], purlBatch)
 
 			for observedProperty in SOS.procedure[proc]:
-				if (observedProperty[:4] == 'http'):
-					obsProperty = URIRef(observedProperty)
-				else:
-					obsProperty = URIRef("{0}/OBSERVED/{1}".format(baseURI, observedProperty.replace(' ','')) ) 
-					# Create a PURL for every observed property URI 
-					CreatePurls([obsProperty], purlBatch)
+				# if (observedProperty[:4] == 'http'):
+				# 	obsProperty = URIRef(observedProperty)
+				# else:
+				obsProperty = URIRef("{0}/OBSERVED/{1}".format(baseURI, observedProperty.replace(' ','').replace('http://','').replace('.','_')) ) 
+				# Create a PURL for every observed property URI 
+				CreatePurls([obsProperty], purlBatch)
 				
 				# check the mapping between observed properties from SOS and as defined by DBPedia 
 				# the collections of samplign features are based on the DBPedia definitions
@@ -108,10 +109,10 @@ def capabilities(SOS):
 				if StandardObsProperty == '':
 					# print "I have no clue what {0} is...".format(obsProperty)
 					continue
-				else: 
-					print obsProperty, "==", StandardObsProperty
+				# else: 
+				# 	print obsProperty, "==", StandardObsProperty
 
-				StandardCollection = URIRef("{0}/FOI_Collection/{1}".format(baseURI, StandardObsProperty) )
+				StandardCollection = URIRef("{0}/FOI_Collection/{1}".format(baseURI, StandardObsProperty).replace(' ','') )
 				if StandardCollection in definedCollections:
 					# the collection is already defined 
 					pass
@@ -134,17 +135,17 @@ def capabilities(SOS):
 				CreatePurls([StandardCollection], purlBatch)
 
 				for i, feature in enumerate(SOS.procedure[proc][observedProperty]['FOI']):
-					sensor = URIRef("{0}/{1}/PROC/{2}/SENSOR/{3}".format(baseURI, SOS.organisation.replace(' ', ''), count, i+1) )
+					sensor = URIRef("{0}/{1}/PROC/{2}/SENSOR/{3}".format(baseURI, SOS.organisation, count, i+1).replace(' ', '') )
 					g.add( ( sensor, RDF.type, prov.Agent ) )
 					g.add( ( sensor, RDF.type, om_lite.process ) ) 
 					g.add( ( sensor, om_lite.procedure, uriProcedure ) )
 					g.add( ( uriProcedure, prov.wasAssociatedWith, sensor ) )
 					g.add( ( sensor, dc.isPartOf, uriSOS) )
 
-					if (feature[:4].lower() == 'http'):
-						FOI = URIRef(feature)
-					else:
-						FOI = URIRef("{0}/{1}/FOI/{2}".format(baseURI, SOS.organisation.replace(' ', ''), feature) )
+					# if (feature[:4].lower() == 'http'):
+					# 	FOI = URIRef(feature)
+					# else:
+					FOI = URIRef("{0}/{1}/FOI/{2}".format(baseURI, SOS.organisation, feature.replace('http://','')).replace(' ', '') )
 
 					g.add( ( FOI, FOAF.name, Literal(feature) ) )
 
@@ -161,7 +162,8 @@ def capabilities(SOS):
 					CreatePurls([FOI, sensor], purlBatch)
 
 					for i, offeringName in enumerate(SOS.featureofinterest[feature]['offerings']):
-						offering = URIRef("{0}/{1}/PROC/{2}/OFFERING/{3}".format(baseURI, SOS.organisation.replace(' ', ''), count, i+1 ) )
+						# print count, offeringName
+						offering = URIRef("{0}/{1}/PROC/{2}/OFFERING/{3}".format(baseURI, SOS.organisation, count, offeringName.replace('.','_') ).replace(' ', '') )
 						# Create a PURL for every offering URI 
 						CreatePurls([offering], purlBatch)
 						
@@ -174,43 +176,62 @@ def capabilities(SOS):
 
 
 	print "Sending triples to endpoint"
-	with progressbar.ProgressBar(max_value=len(g)) as bar:
-		countTriples = 0
-		triples = ""
-		for s,p,o in g.triples((None, None, None)):
-			if str(type(o)) == "<class 'rdflib.term.Literal'>":
-				if o.datatype != None:
-					triples += u'<{0}> <{1}> "{2}"^^<{3}> . \n'.format(s,p,o,o.datatype)
-				else:
-					triples += u'<{0}> <{1}> "{2}" . \n'.format(s,p,o)
-			elif str(type(o)) == "<class 'rdflib.term.URIRef'>":
-				triples += u'<{0}> <{1}> <{2}> . \n'.format(s,p,o)
-			if (countTriples % 100 == 0) and (countTriples > 0):
-				# send data to enpoint
-				query = "INSERT DATA { " + triples + "}"
-				# print query
-				r = requests.post(endpoint, data={'view':'HTML', 'query': query, 'format':'HTML', 'outputformat':'SPARQL/XML' , 'handle':'plain', 'submit':'Update' }) 
-				# print r
-				if str(r) != '<Response [200]>':
-					print "Response: {0}".format(r)
-					# print query
-					print r.text
+	# with progressbar.ProgressBar(max_value=len(g)) as bar:
+		# print "send data to endpoint"
+			# countTriples = 0
+		# triples = ""
+		# for s,p,o in g.triples((None, None, None)):
+		# 	if str(type(o)) == "<class 'rdflib.term.Literal'>":
+		# 		if o.datatype != None:
+		# 			# print o
+		# 			# print o.datatype
+		# 			triples += u'<{0}> <{1}> "{2}"^^<{3}> . \n'.format(s,p,o,o.datatype)
+		# 			# print u'<{0}> <{1}> "{2}"^^<{3}> . \n'.format(s,p,o,o.datatype)
+		# 		else:
+		# 			triples += u'<{0}> <{1}> "{2}" . \n'.format(s,p,o)
+		# 	elif str(type(o)) == "<class 'rdflib.term.URIRef'>":
+		# 		triples += u'<{0}> <{1}> <{2}> . \n'.format(s,p,o)
+		# 	if (countTriples % 100 == 0) and (countTriples > 0):
+		# 		# send data to enpoint
+		# 		query = "INSERT DATA { " + triples + "}"
+		# 		# print query
+		# 		r = requests.post(endpoint, data={'view':'HTML', 'query': query, 'format':'HTML', 'outputformat':'SPARQL/XML' , 'handle':'plain', 'submit':'Update' }) 
+		# 		# print r
+		# 		if str(r) != '<Response [200]>':
+		# 			print "Response: {0}".format(r)
+		# 			# print query
+		# 			print r.text
 				
-				triples = ""
-				countTriples += 1
-			else:
-				countTriples += 1
-			bar.update(countTriples)
+		# 		triples = ""
+		# 		countTriples += 1
+		# 	else:
+		# 		countTriples += 1
+		# 	bar.update(countTriples)
 	# Storing the remaining triples
-	query = "INSERT DATA { " + triples + "}"
-	r = requests.post(endpoint, data={'view':'HTML', 'query': query, 'format':'HTML', 'outputformat':'SPARQL/XML' , 'handle':'plain', 'submit':'Update' }) 
-	# print r
+	# query = "INSERT DATA { " + triples + "}"
+	# r = requests.post(endpoint, data={'view':'HTML', 'query': query, 'format':'HTML', 'outputformat':'SPARQL/XML' , 'handle':'plain', 'submit':'Update' }) 
+	# # print r
+	
+	# Create directory for temporary files 
+	if not os.path.exists('D:/tempFiles/'):
+	    os.makedirs('D:/tempFiles/')
+
+	payload = {'dbname': 'endpoint', 'username': 'Ivo', 'password':'gps', 'port':'5432', 'hostname':'localhost', 'dbengine':'postgis'}
+	session = requests.Session()
+	r = session.post('http://localhost/strabon-endpoint-3.3.2-SNAPSHOT/DBConnect', data=payload)
+	with open('D:/tempFiles/sensors{0}.ttl'.format(SOS.organisation.replace(' ', '')), "w") as f:
+		f.write(g.serialize(format="turtle"))
+	g = Graph()
+	r = session.post(endpoint[:-5]+'Store', data={'view':'HTML', 'format':'Turtle', 'url':'file:///D:/tempFiles/sensors{0}.ttl'.format(SOS.organisation.replace(' ', '')), 'fromurl':'Store from URI' }) 
+
+
+
 	if str(r) != '<Response [200]>':
 		print "Response: {0}".format(r)
 		# print query
 		print r.text
 
-	bar.update(countTriples)
+	# bar.update(countTriples)
 
 	# postPURLbatch(purlBatch,'admin', 'password')
 
